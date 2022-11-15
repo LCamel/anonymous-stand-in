2d926510d59e9e391f0e6ededd1c32568712117c
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-11-15 14:22:31 +0800

    add workaround for gas related errors in the comment

--- a.sh
+++ a.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+#
+# in case that "max fee per gas less than block base fee"
+#
+#anvil --fork-url https://eth-goerli.g.alchemy.com/v2/demo --no-storage-caching
+
 anvil --fork-url https://eth-goerli.g.alchemy.com/v2/demo --fork-block-number 7888888
