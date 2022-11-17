9e2e53939edfe652c991c95a7e58e31e875958d8
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-11-17 16:14:30 +0800

    fix unit tests

--- t.sh
+++ t.sh
@@ -4,7 +4,20 @@
 # https://github.com/semaphore-protocol/semaphore/blob/main/packages/contracts/deployed-contracts/goerli.json
 # 0xe0A452533853310C371b50Bd91BB9DCC8961350F
 
-#time forge test --fork-url https://eth-goerli.g.alchemy.com/v2/demo --fork-block-number 7888655 --libraries=incremental-merkle-tree.sol/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
+# use the anvil test accounts
+export PRIVATE_KEY_0=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+export PRIVATE_KEY_1=0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
+export PRIVATE_KEY_2=0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a
+export PRIVATE_KEY_3=0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6
+export PRIVATE_KEY_4=0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a
+export PRIVATE_KEY_5=0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+export PRIVATE_KEY_6=0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e
+export PRIVATE_KEY_7=0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
+export PRIVATE_KEY_8=0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97
+export PRIVATE_KEY_9=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
+
+
+time forge test --fork-url https://eth-goerli.g.alchemy.com/v2/demo --fork-block-number 7888888 --libraries=incremental-merkle-tree.sol/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
 
 # anvil --fork-url https://eth-goerli.g.alchemy.com/v2/demo --fork-block-number 7888888
-time forge test --fork-url http://127.0.0.1:8545 --fork-block-number 7888888 --libraries=incremental-merkle-tree.sol/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
+#time forge test --fork-url http://127.0.0.1:8545 --fork-block-number 7888888 --libraries=incremental-merkle-tree.sol/Hashes.sol:PoseidonT3:0xe0A452533853310C371b50Bd91BB9DCC8961350F -vv
--- test/AnonymousStandIn.t.sol
+++ test/AnonymousStandIn.t.sol
@@ -3,50 +3,64 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/AnonymousStandIn.sol";
+import "incremental-merkle-tree.sol/Hashes.sol";
 
 contract AnonymousStandInTest is Test {
     AnonymousStandIn public anonymousStandIn;
-    address u_a = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    address u_b = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-    address u_c = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-    address u_d = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
-    address u_e = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
-
-    address s_a = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
-    address s_b = 0x976EA74026E726554dB657fA54763abd0C3a0aa9;
-    address s_c = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955;
-    address s_d = 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f;
-    address s_e = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
 
     function setUp() public {
         anonymousStandIn = new AnonymousStandIn(14491587431966538270500950809981262305760918678010866824965324294330536670835);
     }
 
     function testRegister() public {
+        uint[5] memory userPrivateKeys = [
+            vm.envUint("PRIVATE_KEY_0"),
+            vm.envUint("PRIVATE_KEY_1"),
+            vm.envUint("PRIVATE_KEY_2"),
+            vm.envUint("PRIVATE_KEY_3"),
+            vm.envUint("PRIVATE_KEY_4")
+        ];
+        uint[5] memory asiPrivateKeys = [
+            vm.envUint("PRIVATE_KEY_5"),
+            vm.envUint("PRIVATE_KEY_6"),
+            vm.envUint("PRIVATE_KEY_7"),
+            vm.envUint("PRIVATE_KEY_8"),
+            vm.envUint("PRIVATE_KEY_9")
+        ];
+
         // use stand-in accounts to interact with the contract
-        vm.prank(s_d);
-        anonymousStandIn.register(123);
-        vm.prank(s_e);
-        anonymousStandIn.register(14992967248067620736906915151099623099783214469663224091377866644517376473097);
-        vm.prank(s_a);
-        anonymousStandIn.register(456);
-        vm.prank(s_b);
-        anonymousStandIn.register(789);
+        for (uint i = 0; i < 3; i++) {
+            // generate the question for the userAddr
+            address userAddr = vm.addr(userPrivateKeys[i]);
+            uint secret = i * 10;
+            uint question = PoseidonT3.poseidon([uint160(userAddr), secret]);
+            console.log("question: ", question);
 
-        vm.prank(u_e);
-        anonymousStandIn.proof(
-[uint(0x094e4e09588058b75a6a0b7b764cf369596f8b16082ae70285d66ffb13c142b7),
-uint(0x19197caa3c1b8745cc9b95e8440135200ea6f541951c7b8f0727fdcfc5f2b11b)],
+            // register with the asi addr
+            address asiAddr = vm.addr(asiPrivateKeys[i]);
+            vm.prank(asiAddr);
 
-[[uint(0x260a6e9d4e053d6a6e1be5f7c35388d7ad03be71e8ecb817cf43d481302e7680),
-uint(0x222da4a9509f1b7412b49ff1546844ec292ba7780f7620c7c9b800f242d10f3c)],
-[uint(0x0a8e36f995b31cac55d7a1c1251f4efbde278e415ac0874db6766f05071cb053),
-uint(0x090536dfa53bbd4e52f38df3e5f33988b5a66b799f32db9c54e6b43d24ea78ff)]],
+            anonymousStandIn.register(question);
+        }
 
-[uint(0x2c554665e2eada2cf21b530bb0413e5dec6d6ad639689aa52fc28231676bb5b8),
-uint(0x159b7d219e7c5d8aaaf08704550f5ee1e5ea2a2ecd4177cef28ded9e09360431)],
-
-4
-        );
+        uint[2] memory a = [
+                uint(0x0f74723f7c825c84a3c349465348bf24e36817bb7b0bd8923c8bc293c506268b),
+                uint(0x24abf7977393a42deae055a7b41a42cb244f3d27463747e828b2b60094629ac3)
+            ];
+        uint[2][2] memory b = [
+                [
+                    uint(0x03eae35f730f4f408a4d1a3943b2533af616ad559641d02cccd30e7d2daafb88),
+                    uint(0x2bfdf5e1dbe0767fb485d1319278bdbc4c61d4affbfb7544a7474ac5b9196723)
+                ], [
+                    uint(0x21b9ad8c10bb766e999095efdffa405747f4112fa80a91d87aa61e23a9d264d4),
+                    uint(0x0c47f75362a60f2317a1785784a6516df82b70519a3a9477715e98a041bf355a)
+                ]
+            ];
+        uint[2] memory c = [
+                uint(0x11ec0592969afe0052914b3e34e92c1a713f708132ef5baca1b7303df2262709),
+                uint(0x058e508353430723f44fe602c569d7ca50a4bdd7828f2489d472d0603a5e5fed)
+            ];
+        vm.prank(vm.addr(userPrivateKeys[1]));
+        anonymousStandIn.proof(a, b, c, 1);
     }
 }
