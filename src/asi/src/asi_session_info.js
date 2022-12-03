b74b9e477dd06d4ad390d488fa527972e9d39908
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-12-03 11:18:54 +0800

    fix random logic in SessionInfo

--- src/asi/src/asi_session_info.js
+++ src/asi/src/asi_session_info.js
@@ -1,11 +1,9 @@
 import { ethers } from "ethers"
-
 import { getTree } from "./asi_proof.js";
 
 class SessionInfo {
     static random() {
-        const a = new BigUint64Array(4);
-        return (a[0] << 24n) | (a[1] << 16n) | (a[2] << 8n) | a[3];
+        return ethers.BigNumber.from(ethers.utils.randomBytes(32)).toBigInt();
     }
     // sessionId doesn't have to be a strong-random one
     constructor(sessionId = SessionInfo.random()) {
@@ -30,11 +28,10 @@ class SessionInfo {
     }
 }
 
+/*
 var si = new SessionInfo();
 si.add("B", "0x70997970c51812dc3a010c7d01b50e0d17dc79c8");
 si.add("C", "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc");
 si.add("A", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
 console.log(si.stringify());
-//var j = JSON.parse(si.stringify());
-//console.log()
-//console.log(JSON.stringify(j, undefined, 4));
+*/
