11dc9e0e1456029174a749e504f7a664b9597165
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-12-05 17:21:07 +0800

    separate User and ASI

--- src/asi/src/asi.js
+++ src/asi/src/asi.js
@@ -1,49 +1,71 @@
 import { ethers } from "ethers"
 import { poseidon } from "circomlibjs"; // for getQuestion()
 
+
 // dev: don't hide ethers.js
-class ASI {
-    static ABI = [
-        "event CreateSession(uint indexed sessionId, uint userTreeRoot)",
-        "event Register(uint indexed sessionId, uint question, address standIn)",
-        "event Proof(uint indexed sessionId, address user)",
-        "error ZeroSessionIdError()",
-        "error ZeroUserTreeRootError()",
-        "error ZeroQuestionError()",
-        "error SessionAlreadyExistsError()",
-        "error SessionDoesNotExistError()",
-        "error AlreadyProofedError()",
-        "error VerificationError()",
-        "function createSession(uint sessionId, uint userTreeRoot)",
-        "function getUserTreeRoot(uint sessionId) public view returns (uint)",
-        "function getQuestionTreeRoot(uint sessionId) public view returns (uint)",
-        "function register(uint sessionId, uint question)",
-        "function proof(uint sessionId, uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint userIndex) public returns (bool r)",
-    ];
-    static TEMP_ADDR = "0x00a9a7162107c8119b03c0ce2c9a2ff7bed70c98";
-    /*
-    const { ASI } = require("asi");
-    //const provider = new ethers.providers.JsonRpcProvider(); // local
-    const userSigner = new ethers.Wallet("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", provider);
-    const asiSigner = new ethers.Wallet("0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e", provider);
-    const asi = new ASI(ASI.TEMP_ADDR, userSigner, asiSigner);
-    */
-    // HAS A Contract
-    //static getXXX(){}
-    constructor(contractAddress, userAddress, userSigner, asiSigner) {
-        this.contract = new ethers.Contract(contractAddress, ASI.ABI, userSigner);
+
+const ABI = [
+    "event CreateSession(uint indexed sessionId, uint userTreeRoot)",
+    "event Register(uint indexed sessionId, uint question, address standIn)",
+    "event Proof(uint indexed sessionId, address user)",
+    "error ZeroSessionIdError()",
+    "error ZeroUserTreeRootError()",
+    "error ZeroQuestionError()",
+    "error SessionAlreadyExistsError()",
+    "error SessionDoesNotExistError()",
+    "error AlreadyProofedError()",
+    "error VerificationError()",
+    "function createSession(uint sessionId, uint userTreeRoot)",
+    "function getUserTreeRoot(uint sessionId) public view returns (uint)",
+    "function getQuestionTreeRoot(uint sessionId) public view returns (uint)",
+    "function register(uint sessionId, uint question)",
+    "function proof(uint sessionId, uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint userIndex) public returns (bool r)",
+];
+
+const TEMP_ADDR = "0x00a9a7162107c8119b03c0ce2c9a2ff7bed70c98";
+
+class User {
+    constructor(contractAddress, userAddress, userSigner) {
+        this.contract = new ethers.Contract(contractAddress, ABI, userSigner);
         this.userAddress = userAddress;
         this.userSigner = userSigner;
-        this.asiSigner = asiSigner;
     }
     // create a new session
     // returns a promise of tx
     createSession(sessionId, userTreeRoot) {
         return this.contract.createSession(sessionId, userTreeRoot);
     }
-    // get from the contract on the chain
+    // Promise: get from the contract on the chain
     getUserTreeRoot(sessionId) {
         return this.contract.getUserTreeRoot(sessionId);
+    }
+    // Promise: register events (ethers.js)
+    getRegisterEvents(sessionId) {
+        const filter = this.contract.filters.Register(sessionId);
+        return this.contract.queryFilter(filter);
+    }
+    // Promise: questions (BigInts)
+    getQuestions(sessionId) {
+        return this.getRegisterEvents(sessionId)
+            .then((events) => events.map((event) => event.args.question.toBigInt()));
+    }
+    // Promise: tx
+    proof(sessionId, proofAndPublicSignals) {
+        const { proof, publicSignals } = proofAndPublicSignals;
+        const a = [ proof.pi_a[0], proof.pi_a[1] ];
+        const b = [[ proof.pi_b[0][1], proof.pi_b[0][0] ],
+                   [ proof.pi_b[1][1], proof.pi_b[1][0] ]];
+        const c = [ proof.pi_c[0],  proof.pi_c[1] ];
+        const userIndex = publicSignals[1];
+        return this.contract.proof(sessionId, a, b, c, userIndex);
+    }
+}
+
+class ASI {
+    constructor(contractAddress, userAddress, asiSigner) {
+        this.contract = new ethers.Contract(contractAddress, ABI, asiSigner);
+        this.userAddress = userAddress;
+        this.asiSigner = asiSigner;
     }
     // Promise: secret = signByAsi(sessionId)
     getOpinionatedSecret(sessionId) {
@@ -61,27 +83,10 @@ class ASI {
             ;
     }
     // you have to make sure that you are in the user tree before calling register
+    // if not, you will not be able to call prove() to get back the fund
     register(sessionId, question) {
         return this.contract.connect(this.asiSigner).register(sessionId, question);
     }
-    // promise of register events
-    getRegisterEvents(sessionId) {
-        const filter = this.contract.filters.Register(sessionId);
-        return this.contract.queryFilter(filter);
-    }
-    // promise of questions (BigInts)
-    getQuestions(sessionId) {
-        return this.getRegisterEvents(sessionId)
-            .then((events) => events.map((event) => event.args.question.toBigInt()));
-    }
-    proof(sessionId, proofAndPublicSignals) {
-        const { proof, publicSignals } = proofAndPublicSignals;
-        const a = [ proof.pi_a[0], proof.pi_a[1] ];
-        const b = [[ proof.pi_b[0][1], proof.pi_b[0][0] ],
-                   [ proof.pi_b[1][1], proof.pi_b[1][0] ]];
-        const c = [ proof.pi_c[0],  proof.pi_c[1] ];
-        const userIndex = publicSignals[1];
-        return this.contract.proof(sessionId, a, b, c, userIndex);
-    }
 }
-export { ASI };
+
+export { ASI, User, TEMP_ADDR };
