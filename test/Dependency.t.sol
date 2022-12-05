132ef373cf0562f544d1fb8f314806b4fef9800e
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-12-05 15:18:39 +0800

    add a test case for IncrementalMerkleTree

--- test/Dependency.t.sol
+++ test/Dependency.t.sol
@@ -6,6 +6,8 @@ import { PoseidonT3 } from "incremental-merkle-tree.sol/Hashes.sol";
 import "incremental-merkle-tree.sol/IncrementalBinaryTree.sol";
 
 contract DependencyTest is Test {
+    using IncrementalBinaryTree for IncrementalTreeData;
+
     function testPoseidon() public {
         //assertEq(PoseidonT3.poseidon([uint(1), uint(2)]), 7853200120776062878684798364095072458815029376092732009249414926327459813530);
         if (PoseidonT3.poseidon([uint(1), uint(2)]) != 7853200120776062878684798364095072458815029376092732009249414926327459813530) {
@@ -22,5 +24,21 @@ contract DependencyTest is Test {
                 ;
             fail(errorMsg);
         }
+    }
+    /*
+    var { poseidon } = require("circomlibjs");
+    var { IncrementalMerkleTree } = require("@zk-kit/incremental-merkle-tree");
+    t = new IncrementalMerkleTree(poseidon, 5, 0, 2);
+    console.log(t.root); // 19712377064642672829441595136074946683621277828620209496774504837737984048981n
+    t.insert(BigInt(1));
+    console.log(t.root); // 2198688200817217279804184813425808109663645328240240708703506126151229416864n
+
+    */
+    IncrementalTreeData tree;
+    function testIncrementalMerkleTree() public {
+        tree.init(5, 0);
+        assertEq(tree.root, 19712377064642672829441595136074946683621277828620209496774504837737984048981);
+        tree.insert(1);
+        assertEq(tree.root, 2198688200817217279804184813425808109663645328240240708703506126151229416864);
     }
 }
