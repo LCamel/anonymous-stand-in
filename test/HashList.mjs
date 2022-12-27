88ccd733286398a038f396e6ba7766e6c8d875f0
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-12-27 10:31:03 +0800

    class HashList

--- test/HashList.mjs
+++ test/HashList.mjs
@@ -1,24 +1,38 @@
 "use strict";
 import { poseidon } from "circomlibjs";
 
-const HASH_COUNT = 6;
-const HASH_INPUT_COUNT = 4;
-const MAX_INPUT_COUNT = 1 + HASH_COUNT * (HASH_INPUT_COUNT - 1);
-
-var inputs = new Array(MAX_INPUT_COUNT);
-for (let i = 0; i < inputs.length; i++) {
-    inputs[i] = BigInt(i);
+class HashList {
+    constructor(hashWidth) {
+        this.hashWidth = hashWidth;
+        this.length = 0;
+        this.buf = new Array(hashWidth);
+    }
+    add(item) {
+        const len = this.length;
+        if (len <= 1) {
+            this.buf[len] = BigInt(item);
+        } else {
+            const index = (len - 1) % (this.hashWidth - 1) + 1;
+            if (index == 1) {
+                this.buf[0] = poseidon(this.buf);
+            }
+            this.buf[index] = BigInt(item);
+        }
+        this.length = len + 1;
+    }
+    hash() {
+        // 0 1 2 3 4 5 6 7 8 9 10
+        // 0 1 2 3 4 2 3 4 2 3 4
+        const len = this.length;
+        const bound = (len <= 1) ? len : (len - 2) % (this.hashWidth - 1) + 2;
+        const tmp = new Array(this.hashWidth);
+        for (let i = 0; i < this.hashWidth; i++) {
+            tmp[i] = (i < bound) ? this.buf[i] : BigInt(0);
+        }
+        return poseidon(tmp);
+    }
 }
 
-var outs = new Array(HASH_COUNT);
-for (let h = 0; h < HASH_COUNT; h++) {
-    const i0 = (h == 0) ? inputs[0] : outs[h - 1];
-    const start = 1 + (HASH_INPUT_COUNT - 1) * h;
-    outs[h] = poseidon([i0, ...inputs.slice(start, start + (HASH_INPUT_COUNT - 1))]);
-    console.log(outs[h]);
-}
-
-console.log("==========");
 
 /*
 var h = new Array(HASH_COUNT);
@@ -36,16 +50,44 @@ h[5] = poseidon([h[4], 16, 17, 18]);
 console.log(h[5]);
 */
 
-var h = new Array(HASH_COUNT);
-h[0] = poseidon([0, 1, 2, 3]);
-console.log(h[0]);
-h[1] = poseidon([h[0], 4, 5, 6]);
-console.log(h[1]);
-h[2] = poseidon([h[1], 7, 0, 0]);
-console.log(h[2]);
-h[3] = poseidon([h[2], 0, 0, 0]);
-console.log(h[3]);
-h[4] = poseidon([h[3], 0, 0, 0]);
-console.log(h[4]);
-h[5] = poseidon([h[4], 0, 0, 0]);
-console.log(h[5]);
+function eq(v1, v2) {
+    if (v1 != v2) throw "bad value: " + v1 + " " + v2;
+}
+
+console.log("=======");
+const hl = new HashList(4);
+eq(hl.hash(), poseidon([0, 0, 0, 0]));
+hl.add(0);
+eq(hl.hash(), poseidon([0, 0, 0, 0]));
+hl.add(1);
+eq(hl.hash(), poseidon([0, 1, 0, 0]));
+hl.add(2);
+eq(hl.hash(), poseidon([0, 1, 2, 0]));
+hl.add(3);
+eq(hl.hash(), poseidon([0, 1, 2, 3]));
+
+hl.add(4);
+eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 0, 0]));
+hl.add(5);
+eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 5, 0]));
+hl.add(6);
+eq(hl.hash(), poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]));
+
+hl.add(7);
+eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 0, 0]));
+hl.add(8);
+eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 0]));
+hl.add(9);
+eq(hl.hash(), poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 9]));
+
+hl.add(10);
+eq(hl.hash(), poseidon([poseidon([poseidon([poseidon([0, 1, 2, 3]), 4, 5, 6]), 7, 8, 9]), 10, 0, 0]));
+
+
+const hl2 = new HashList(4);
+hl2.add(4);
+hl2.add(5);
+hl2.add(6);
+hl2.add(7);
+hl2.add(8);
+eq(hl2.hash(), poseidon([poseidon([4, 5, 6, 7]), 8, 0, 0]));
